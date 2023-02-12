09d4a86e67a90427ed1b0a5cd6a04f46cc1040e7
Jason Bock <jason.r.bock@outlook.com>
2023-02-11 19:03:45 -0600

    got a playwright test in

---
+++ src/Jason/BlazingCollatz/BlazingCollatz.WebTests/PlaywrightTests.cs
@@ -0,0 +1,31 @@
+﻿using System.Text.RegularExpressions;
+using Microsoft.Playwright;
+using Microsoft.Playwright.NUnit;
+
+namespace PlaywrightTests;
+
+[Parallelizable(ParallelScope.Self)]
+[TestFixture]
+public class PlaywrightTests : PageTest
+{
+	[Test]
+	public async Task HomepageHasPlaywrightInTitleAndGetStartedLinkLinkingtoTheIntroPageAsync()
+	{
+		await this.Page.GotoAsync("https://playwright.dev");
+
+		// Expect a title "to contain" a substring.
+		await this.Expect(this.Page).ToHaveTitleAsync(new Regex("Playwright"));
+
+		// create a locator
+		var getStarted = this.Page.GetByRole(AriaRole.Link, new() { Name = "Get started" });
+
+		// Expect an attribute "to be strictly equal" to the value.
+		await this.Expect(getStarted).ToHaveAttributeAsync("href", "/docs/intro");
+
+		// Click the get started link.
+		await getStarted.ClickAsync();
+
+		// Expects the URL to contain intro.
+		await this.Expect(this.Page).ToHaveURLAsync(new Regex(".*intro"));
+	}
+}
---
+++ src/Jason/BlazingCollatz/BlazingCollatz.WebTests/WebAssemblySiteTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Playwright;
+using Microsoft.Playwright.NUnit;
+
+namespace PlaywrightTests;
+
+[Parallelizable(ParallelScope.Self)]
+public sealed class WebAssemblySiteTests 
+	: PageTest
+{
+	[Test]
+	public async Task NavigateToSequenceAsync()
+	{
+		await this.Page.GotoAsync("http://localhost:5085");
+
+		await this.Expect(this.Page).ToHaveTitleAsync("BlazingCollatz.WebAssemblyHost");
+
+		var sequenceLink = this.Page.GetByRole(AriaRole.Link, new() { Exact = true, Name = "Sequence" });
+		await sequenceLink.ClickAsync();
+
+		await this.Expect(sequenceLink).ToHaveAttributeAsync("href", "sequence");
+		await this.Expect(this.Page).ToHaveURLAsync("http://localhost:5085/sequence");
+	}
+}
