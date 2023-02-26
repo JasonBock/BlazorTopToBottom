5998cfbf2dea4bbceb99c28e312676ca0294a781
Jason Bock <jason.r.bock@outlook.com>
2023-02-26 15:34:07 -0600

    Updated package versions

--- src/Jason/BlazingCollatz/BlazingCollatz.ConsoleApplication/Program.cs
+++ src/Jason/BlazingCollatz/BlazingCollatz.ConsoleApplication/Program.cs
@@ -1,1 +1,6 @@
+using System.Runtime.Versioning;
+
+// This gets around CA1416 giving a warning about using JSExportAttribute
+[assembly: SupportedOSPlatform("browser")]
+
 Console.WriteLine("BlazingCollatz.ConsoleApplication");
--- src/Jason/BlazingCollatz/BlazingCollatz.HtmlApplication/Program.cs
+++ src/Jason/BlazingCollatz/BlazingCollatz.HtmlApplication/Program.cs
@@ -1,1 +1,6 @@
-﻿Console.WriteLine("BlazingCollatz.HtmlApplication");
+﻿// This gets around CA1416 giving a warning about using JSExportAttribute
+using System.Runtime.Versioning;
+
+[assembly: SupportedOSPlatform("browser")]
+
+Console.WriteLine("BlazingCollatz.HtmlApplication");
