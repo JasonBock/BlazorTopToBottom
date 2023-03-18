5bc5ef9c395e6d6b4fffdbd7bd314cb86c73d2db
Jason Bock <jason.r.bock@outlook.com>
2023-03-18 15:04:37 -0500

    WASI fun!

---
+++ src/Jason/BlazingCollatz/BlazingCollatz.WasiApplication/Program.cs
@@ -0,0 +1,11 @@
+﻿using Collatz;
+using System.Numerics;
+
+var start = new BigInteger(444);
+var sequence = CollatzSequenceGenerator.Generate<BigInteger>(start);
+
+Console.WriteLine($"Starting value: {start}");
+Console.WriteLine();
+Console.WriteLine("Resulting sequence:");
+Console.WriteLine();
+Console.WriteLine(string.Join(", ", sequence));
