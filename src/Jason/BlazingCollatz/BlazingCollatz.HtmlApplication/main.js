cdb43db0e30949be3b699889af1133a68eff1bee
Jason Bock <jason.r.bock@outlook.com>
2023-03-18 09:28:11 -0500

    Hey! I think I got threads working in the HTML example

--- src/Jason/BlazingCollatz/BlazingCollatz.HtmlApplication/main.js
+++ src/Jason/BlazingCollatz/BlazingCollatz.HtmlApplication/main.js
@@ -8,7 +8,14 @@ const { setModuleImports, getAssemblyExports, getConfig } = await dotnet
 const config = getConfig();
 const dotNetExports = await getAssemblyExports(config.mainAssemblyName);
 
-
 export function getSequence(start) {
     return dotNetExports.CollatzInterop.Generate(start);
+}
+
+export function findLongestSequenceSequentially() {
+    return dotNetExports.CollatzInterop.FindLongestSequenceSequentially();
+}
+
+export function findLongestSequenceInParallel() {
+    return dotNetExports.CollatzInterop.FindLongestSequenceInParallel();
 }
