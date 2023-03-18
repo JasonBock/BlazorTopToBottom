8edcb6c4780ce8abc8951a87e9106dbf26c15862
Jason Bock <jason.r.bock@outlook.com>
2023-03-17 19:26:14 -0500

    Updating for AOT (and removing some gRPC stuff)

--- src/Jason/BlazingCollatz/BlazingCollatz.WebComponents/Extensions/IServiceCollectionExtensions.cs
+++ src/Jason/BlazingCollatz/BlazingCollatz.WebComponents/Extensions/IServiceCollectionExtensions.cs
@@ -1,6 +1,4 @@
 ﻿using Blazored.LocalStorage;
-using Grpc.Net.Client;
-using Grpc.Net.Client.Web;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace BlazingCollatz.WebComponents.Extensions;
@@ -10,12 +8,6 @@ public static class IServiceCollectionExtensions
 	public static void AddWebComponents(this IServiceCollection self)
 	{
 		self.AddTransient<SequenceViewModel>();
-		self.AddSingleton(services =>
-		{
-			var httpClient = new HttpClient(new GrpcWebHandler(GrpcWebMode.GrpcWeb, new HttpClientHandler()));
-			var channel = GrpcChannel.ForAddress("http://localhost:5060", new GrpcChannelOptions { HttpClient = httpClient });
-			return new CollatzGrpc.CollatzServiceDefinition.CollatzServiceDefinitionClient(channel);
-		});
 		self.AddBlazoredLocalStorage();
 	}
 }
