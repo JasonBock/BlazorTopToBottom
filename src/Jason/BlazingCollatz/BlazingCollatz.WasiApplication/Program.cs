fc2499ca5a817d47459f28f323ef00c78cb7b143
Jason Bock <jason.r.bock@outlook.com>
2023-03-18 18:12:10 -0500

    Added a WASM generator

--- src/Jason/BlazingCollatz/BlazingCollatz.WasiApplication/Program.cs
+++ src/Jason/BlazingCollatz/BlazingCollatz.WasiApplication/Program.cs
@@ -1,11 +1,36 @@
 ﻿using Collatz;
 using System.Numerics;
 
-var start = new BigInteger(444);
-var sequence = CollatzSequenceGenerator.Generate<BigInteger>(start);
+// Remember to run:
+//
+// wasmtime --dir=. BlazingCollatz.WasiApplication.wasm
+//
+// to "see" this file
+if (File.Exists("value.txt"))
+{
+	if (BigInteger.TryParse(File.ReadAllText("value.txt"), out var start))
+	{
+		var sequence = CollatzSequenceGenerator.Generate<BigInteger>(start);
 
-Console.WriteLine($"Starting value: {start}");
-Console.WriteLine();
-Console.WriteLine("Resulting sequence:");
-Console.WriteLine();
-Console.WriteLine(string.Join(", ", sequence));
+		Console.WriteLine($"Starting value: {start}");
+		Console.WriteLine();
+		Console.WriteLine("Resulting sequence:");
+		Console.WriteLine();
+		Console.WriteLine(string.Join(", ", sequence));
+	}
+	else
+	{
+		Console.WriteLine("Could not parse content in value.txt");
+	}
+}
+else
+{
+	var start = new BigInteger(444);
+	var sequence = CollatzSequenceGenerator.Generate<BigInteger>(start);
+
+	Console.WriteLine($"Starting value: {start}");
+	Console.WriteLine();
+	Console.WriteLine("Resulting sequence:");
+	Console.WriteLine();
+	Console.WriteLine(string.Join(", ", sequence));
+}
