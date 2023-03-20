7e19e82b4633b374b664bc942e98b7d255947d43
Rockford Lhotka <rocky@lhotka.net>
2023-03-20 11:46:48 -0700

    Add device API example

---
+++ src/Rocky/DeviceAPIAccess/DeviceAPIAccess/Platforms/Android/Services/GetText.cs
@@ -0,0 +1,12 @@
+﻿using DeviceAPIAccess.Services;
+
+namespace DeviceAPIAccess.Platforms.Services
+{
+  public class GetText : IGetText
+  {
+    string IGetText.GetText()
+    {
+      return "Hello from Android";
+    }
+  }
+}
