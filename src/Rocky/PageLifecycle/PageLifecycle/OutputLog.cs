a7053affa932f5d3ec82ef7e2e49e909e5cf16e1
Rockford Lhotka <rocky@lhotka.net>
2024-03-04 10:11:09 -0800

    Add PageLifecycle demo

---
+++ src/Rocky/PageLifecycle/PageLifecycle/OutputLog.cs
@@ -0,0 +1,12 @@
+﻿namespace PageLifecycle
+{
+  public class OutputLog
+  {
+    public List<string> Data = new List<string>();
+
+    public void Log(string message)
+    {
+      Data.Add(message);
+    }
+  }
+}
