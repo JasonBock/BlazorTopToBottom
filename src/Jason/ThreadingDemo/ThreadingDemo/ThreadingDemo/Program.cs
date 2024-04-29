b95c54b329385c40d066e49c909b11a41e851d71
Jason Bock <jason.r.bock@outlook.com>
2024-04-29 10:35:53 -0500

    Added a threading demo for .NET 9

---
+++ src/Jason/ThreadingDemo/ThreadingDemo/ThreadingDemo/Program.cs
@@ -0,0 +1,29 @@
+using ThreadingDemo.Client.Pages;
+using ThreadingDemo.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+}
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveWebAssemblyRenderMode(options => options.ServeMultithreadingHeaders = true)
+    .AddAdditionalAssemblies(typeof(ThreadingDemo.Client._Imports).Assembly);
+
+app.Run();
