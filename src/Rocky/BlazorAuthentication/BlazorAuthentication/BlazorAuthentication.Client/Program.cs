2c506ffebd35496e95e60c7759d888ad20c54ad6
Rockford Lhotka <rocky@lhotka.net>
2024-04-30 21:54:26 -0500

    Configure client for authz and authn

--- src/Rocky/BlazorAuthentication/BlazorAuthentication/BlazorAuthentication.Client/Program.cs
+++ src/Rocky/BlazorAuthentication/BlazorAuthentication/BlazorAuthentication.Client/Program.cs
@@ -1,5 +1,10 @@
+using BlazorAuthentication.Client;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+builder.Services.AddAuthorizationCore();
+builder.Services.AddScoped<AuthenticationStateProvider, ServerAuthenticationStateProvider>();
 
 await builder.Build().RunAsync();
---
+++ src/Rocky/BlazorAuthentication/BlazorAuthentication/BlazorAuthentication.Client/ServerAuthenticationStateProvider.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
+
+namespace BlazorAuthentication.Client
+{
+    public class ServerAuthenticationStateProvider : AuthenticationStateProvider
+    {
+        public async override Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            var anonymous = new ClaimsIdentity();
+            return await Task.FromResult(new AuthenticationState(new ClaimsPrincipal(anonymous)));
+        }
+    }
+}
